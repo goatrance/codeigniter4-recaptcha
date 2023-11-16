cb9a2e4765b55d73a1b3242406505d075bbeb550
Peltekian Róbert <info@dualp.hu>
2023-11-16 13:56:49 +0100

    renew token

--- Views/init_v3.php
+++ Views/init_v3.php
@@ -1,16 +1,24 @@
-<?php if($init):?>
+<?php if ($init) : ?>
 
-<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?render=<?= $key;?>"></script>
+    <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?render=<?= $key; ?>"></script>
 
-<?php endif;?>
+<?php endif; ?>
 
 <script type="text/javascript">
+    grecaptcha.ready(function() {
+        grecaptcha.execute('<?= $key; ?>', <?= json_encode($options); ?>).then(function(token) {
+            document.getElementById('<?= $id; ?>').value = token;
 
-grecaptcha.ready(function() {
-    grecaptcha.execute('<?= $key;?>', <?= json_encode($options);?>).then(function(token) {
-        document.getElementById('<?= $id;?>').value = token;
-        document.getElementById('<?= $id;?>').oninput();
+        });
     });
-});
 
+    setInterval(function() {
+        grecaptcha.ready(function() {
+            grecaptcha.execute('<?= $key; ?>', {
+                action: '<?= $options['action'] ?? '' ?>'
+            }).then(function(e) {
+                $('#<?= $id; ?>').val(e);
+            });
+        });
+    }, 90 * 1000);
 </script>
